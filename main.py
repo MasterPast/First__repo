b783936eef088e45cfd374927e4078671505e45e
MasterPast <igory733@gmail.com>
2023-08-01 16:22:13 +0300

    bankomat

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-print("HELLO WORLD Fuck")
+pin = 1122
+tr = 0
 
-print("YEAH")
-
-print (input("What`s name, man?"),"FUCK")
-
-
+while tr < 3:
+    inpin = input("PIN Code? ")
+    if str(inpin) != str(pin):
+        tr += 1
+        if tr == 3:
+            print("Your card is blocked.")
+        else:
+            print(f"Try again. Remain {3-tr} tries.") 
+    else:
+        print("Welcome to system.")
+        tr = 3
