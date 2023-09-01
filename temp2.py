8c437a5c1fc52ba643d835741aba29df4d25232c
MasterPast <igory733@gmail.com>
2023-09-01 12:03:40 +0300

    1

--- temp2.py
+++ temp2.py
@@ -1,26 +1,16 @@
-CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
-TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
-               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
-TRANS = {}
-qqq = {ord('г') : 'G', ord('м') : 'W'}
+from datetime import datetime
+import pytz
 
+month = 3
+year = 2021
+# date = '2021-05-27 17:08:34.149Z'
 
-def translate(name):
+date = '2021-05-27 17:08:34.149Z'
 
-    tran = {}
-    cyr = []  
-    
-    for symb in CYRILLIC_SYMBOLS:
-        m_symb = ord(f'{symb}')
-        cyr += [m_symb]    
-
-    for i in range(len(TRANSLATION)):
-        tran[cyr[i]] = TRANSLATION[i]
-    print(tran)
-    name1 = print(name.translate(tran))
-    return(name1)
+date = date[:-5:]
+print(type(date))
+print(date)
+date_in = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+print(date_in.strftime('%A %d %B %Y'))
     
     
-
-translate('плотников')
-
