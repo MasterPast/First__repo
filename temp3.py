d65dd637b59e42452b4142646ca17bf25fe750a6
MasterPast <igory733@gmail.com>
2023-09-05 09:15:13 +0300

    last

---
+++ temp3.py
@@ -0,0 +1,11 @@
+def discount_price(discount):
+    res = 100 - discount * 100
+    return res
+
+def
+
+price = 100
+print(1-0.16)
+cost_10 = discount_price(0.1)
+print(discount_price(0.29))
+print(cost_10(price))
