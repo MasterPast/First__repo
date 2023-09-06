fd8c6d985b76a29894b767f09a079fb329f92b15
MasterPast <igory733@gmail.com>
2023-09-06 21:40:07 +0300

    cnt0609

--- temp3.py
+++ temp3.py
@@ -2,22 +2,25 @@ import re
 
 def generator_numbers(string=""):
     print(1)
-    
+    sum = 0
     pr = re.findall('[0-9]+', string)
     print(pr)
     for numb in pr:
-        sum_profit(numb)
-    yield numb
-        
-        
+        sum += sum_profit(numb)
+        print(sum)
+    yield sum
                     
 
 def sum_profit(string):
     
-    sum += int(string)
-    print(sum)
+    sum = int(string)
+    # print(sum)
     return sum
         
 st = 'The resulting profit was: from the southern possessions $ 100, from the northern colonies $500, and the king gave $1000.'
+st2 = 'The resulting profit was: from the southern possessions $ 10, from the northern colonies $50, and the king gave $100'
 
-next(generator_numbers(st))
+aq = next(generator_numbers(st2))
+
+print (f'>>> {aq}')
+print (f'>>> {aq}')
